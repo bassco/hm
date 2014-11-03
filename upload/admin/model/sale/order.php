a45888ccfd2f2ed707ffdc15a3584564125d1d05
Andrew Basson <andrew@bassco.co.za>
2014-11-03 16:03:07 +0200

    VAT changes

--- upload/admin/model/sale/order.php
+++ upload/admin/model/sale/order.php
@@ -581,7 +581,7 @@ date) VALUES (
 	}
 
 	public function getOrderTotals($order_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_total WHERE order_id = '" . (int)$order_id . "' ORDER BY sort_order");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_total` WHERE order_id = '" . (int)$order_id . "' ORDER BY sort_order");
 
 		return $query->rows;
 	}
